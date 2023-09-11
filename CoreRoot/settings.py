2e780cceb2f9ec498ee231ad89f598cd0ff5a546
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-09-11 11:17:11 +0900

    데이터베이스연동

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -75,12 +75,14 @@ WSGI_APPLICATION = 'CoreRoot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'coredb',
+        'USER': 'core',
+        'PASSWORD': 'wCh29&HE&T83',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko'
 
 TIME_ZONE = 'UTC'
 
