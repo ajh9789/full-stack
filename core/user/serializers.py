22dcfad379b5297d32a0ffb8fc471247c1fe2fb1
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-09-12 09:20:11 +0900

    api 작동확인

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+
 from core.user.models import User
+
 
 class UserSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(source='public_id', read_only=True, format='hex')
@@ -8,18 +10,6 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = [
-            'id',
-            'username',
-            'first_name',
-            'last_name',
-            'bio',
-            'avatar',
-            'email',
-            'is_active',
-            'created',
-            'updated',
-        ]
-        read_only_fields = [
-            'is_active',
-        ]
+        fields = ['id', 'username', 'first_name', 'last_name',
+                  'bio', 'avatar', 'email', 'is_active', 'created', 'updated']
+        read_only_field = ['is_active']
